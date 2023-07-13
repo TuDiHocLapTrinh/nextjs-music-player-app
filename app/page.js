b65eed2537cd373fc3352d4590b1d2eba09f6500
Anh Tu Dang <anhtudang95@gmail.com>
2023-07-13 22:39:20 +0900

    init commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,157 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import Image from 'next/image';
+import styles from './page.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBackward,
+  faPlay,
+  faForward,
+  faPause,
+} from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState, useRef } from 'react';
+
+const songs = [
+  {
+    path: './assets/1.mp3',
+    displayName: "Ther Charmer's Call",
+    cover: './assets/1.jpg',
+    artist: 'Hanu Dixit',
+  },
+  {
+    path: './assets/2.mp3',
+    displayName: 'Yo Will Never See Me Coming',
+    cover: './assets/2.jpg',
+    artist: 'NEFFEX',
+  },
+  {
+    path: './assets/3.mp3',
+    displayName: 'Intellect',
+    cover: './assets/3.jpg',
+    artist: 'Young Logos',
+  },
+];
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [musicIndex, setMusicIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const [progressWidth, setProgressWidth] = useState('0%');
+  const [durationText, setDurationText] = useState('00:00');
+  const [currentTimeText, setCurrentTimeText] = useState('00:00');
+
+  const audioRef = useRef();
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying]);
+
+  const togglePlay = () => setIsPlaying(!isPlaying);
+
+  const changeMusic = (direction) => {
+    const newMusicIndex =
+      (musicIndex + direction + songs.length) % songs.length;
+    setMusicIndex(newMusicIndex);
+  };
+
+  function updateProgressBar(e) {
+    const { duration, currentTime } = e.target;
+    const progressPercent = (currentTime / duration) * 100;
+    setProgressWidth(`${progressPercent}%`);
+
+    const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
+    const durationText = `${formatTime(duration / 60)}:${formatTime(
+      duration % 60
+    )}`;
+    setDurationText(durationText);
+    const currentTimeText = `${formatTime(currentTime / 60)}:${formatTime(
+      currentTime % 60
+    )}`;
+    setCurrentTimeText(currentTimeText);
+  }
+
+  // const setProgressBar = (e) => {
+  //   console.log(e);
+  //   const width = e.target.clientWidth;
+  //   const clickX = e.offsetX;
+  //   audioRef.currentTime = (clickX / width) * audioRef.duration;
+  // };
+
+  return (
+    <>
+      <div className={styles.background}>
+        <img
+          src={songs[musicIndex].cover}
+          id='bg-img'
+          width={500}
+          height={500}
         />
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <div className={styles.container}>
+        <div className={styles.playerImg}>
+          <img
+            src={songs[musicIndex].cover}
+            className={styles.active}
+            id='cover'
+            width={500}
+            height={500}
+          />
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+        <h2 id='music-title' className={styles.h2}>
+          {songs[musicIndex].displayName}
+        </h2>
+        <h3 id='music-artist' className={styles.h3}>
+          {songs[musicIndex].artist}
+        </h3>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          className={styles.playerProgress}
+          id='player-progress'
+          onClick={(e) => setProgressBar(e)}
         >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+          <div
+            className={styles.progress}
+            id='progress'
+            style={{ width: progressWidth }}
+          ></div>
+          <div className={styles.musicDuration}>
+            <span id='current-time'>{durationText}</span>
+            <span id='duration'>{currentTimeText}</span>
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className={styles.playerControls}>
+          <FontAwesomeIcon
+            icon={faBackward}
+            className={styles.faSolid}
+            onClick={() => changeMusic(-1)}
+          />
+          <FontAwesomeIcon
+            icon={isPlaying === true ? faPause : faPlay}
+            className={`${styles.faSolid} ${styles.playButton}`}
+            onClick={togglePlay}
+          />
+          <FontAwesomeIcon
+            icon={faForward}
+            className={styles.faSolid}
+            onClick={() => changeMusic(1)}
+          />
+        </div>
+
+        <audio
+          allow='autoplay'
+          src={songs[musicIndex].path}
+          ref={audioRef}
+          onTimeUpdate={(e) => updateProgressBar(e)}
+          onEnded={() => changeMusic(1)}
+        ></audio>
       </div>
-    </main>
-  )
+    </>
+  );
 }
